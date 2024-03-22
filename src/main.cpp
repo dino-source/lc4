150fc01dcd9c287d94cfd07e288a10895a3320d9
Dino Source <dinosource.cpp@gmail.com>
2024-03-22 14:34:24 -0700

    Add some example code in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include "solution.h"
 
+#include <iostream>
+#include <vector>
+
+
 int main() {
     [[maybe_unused]] Solution sol;
+    std::vector<int> a {1, 2};
+    std::vector<int> b {3, 4};
+    auto res {sol.findMedianSortedArrays(a, b)};
+    std::cout << res << '\n';
 }
