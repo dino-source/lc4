2331cad2ece3b6421a208b6b2161e767e4fe3c1c
Dino Source <dinosource.cpp@gmail.com>
2024-03-22 15:15:09 -0700

    Clean up scripts

--- xconfig.sh
+++ xconfig.sh
@@ -4,8 +4,8 @@
 /usr/bin/cmake --no-warn-unused-cli \
     -DCMAKE_BUILD_TYPE:STRING=Debug \
     -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
-    -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc \
-    -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ \
+    -DCMAKE_C_COMPILER:FILEPATH=$C_COMPILER \
+    -DCMAKE_CXX_COMPILER:FILEPATH=$CXX_COMPILER \
     -S$DEVELOP/lc4 \
     -B$DEVELOP/lc4/build \
     -G Ninja
